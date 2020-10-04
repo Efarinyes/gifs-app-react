5b054778a4e4fc7cf2166e4b35c4d8eac41f3778
Efarinyes <efarinyes@gmail.com>
2020-10-04 18:55:13 +0200

    Primer Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch']); 
+
+    // const afegirCategoria = () => {
+    //     seCategories([...categories, 'Sony'])
+    //     setCategories( cats => [...cats, 'Sony']) 
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories = { setCategories }/>
+            <hr/>
+            <ol>
+                { categories.map( categoria =>( 
+                    <GifGrid 
+                        key= {categoria} 
+                        categoria = { categoria }/>) )
+                }
+            </ol>
+            
+        </>
+    )
+}
---
+++ src/components/GifGrid.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
+
+
+export const GifGrid = ( {categoria} ) => {
+    
+    const { data: images, loading} = useFetchGifs( categoria )
+
+    // CODI FUNCIONAL SENSE EL custom hook useFetchGifs
+    // LA NETEJA DEL COMPONENT ES CONSIDERABLE
+
+
+    //  const apiKey = 'P57qF7KBebhmbZERLpvjiEa2TVdIBbBM'
+    //  const url = 'https://api.giphy.com/v1/gifs/search/'
+    //  const categoriaConsulta = { categoria }
+
+    // const cercaGifs = async() => {
+        
+    //     const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(categoria) }&limit=10&api_key=P57qF7KBebhmbZERLpvjiEa2TVdIBbBM`;
+    //     const resp = await fetch( url );
+    //     const {data} = await resp.json();
+    //     const gif = data.map( img => {
+    //         return {
+    //             id: img.id,
+    //             title: img.title,
+    //             url: img.images?.downsized_medium.url
+    //         }
+    //     })
+    //     console.log(gif);
+    //     setimages( gif );
+    // }
+   // cercaGifs()
+
+    return (
+        <>
+         <h3 className="animate__animated animate__fadeIn"> {categoria} </h3>
+         
+        { loading && <p className ="animate__animated' animate__flash">Carregant...</p>}
+          <div className='card-grid'>
+                    {
+                        images.map( img => (
+                            <GifGridItem 
+                                key = { img.id}
+                                {...img}
+                            />
+                        ))
+                    }
+                
+                
+            </div> 
+        </>
+    )
+}
+
+
