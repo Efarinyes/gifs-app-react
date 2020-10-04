32e6d61410c5ecc4b814855ece6a0998b9665906
Efarinyes <efarinyes@gmail.com>
2020-10-04 18:57:09 +0200

    docs afegit

---
+++ docs/precache-manifest.6e4bc8f1360f5617ac9a98ab79654a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e331a806e09ab757a1083e3db7bfaef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e99c94a915396579d457",
+    "url": "/static/css/main.d77a6e3f.chunk.css"
+  },
+  {
+    "revision": "d3e4fd402a25d941debf",
+    "url": "/static/js/2.68859a0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68859a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99c94a915396579d457",
+    "url": "/static/js/main.c87f0810.chunk.js"
+  },
+  {
+    "revision": "1049da53b40edaf3a561",
+    "url": "/static/js/runtime-main.98d75cec.js"
+  }
+]);
